59fc014ea115df4b7ff63357e800570bd04de9cd
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-05-31 15:13:08 +0300

    Init

---
+++ src/main/java/com/example/sync_book/SyncBookApplication.java
@@ -0,0 +1,12 @@
+package com.example.sync_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncBookApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/sync_book/SyncBookApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.sync_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncBookApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
