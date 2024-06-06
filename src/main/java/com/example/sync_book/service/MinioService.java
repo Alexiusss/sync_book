d1251d95b5331bcd5c48156c16a1c26dd2108268
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-06-06 17:54:03 +0300

    Added logging to MinioService

--- src/main/java/com/example/sync_book/service/MinioService.java
+++ src/main/java/com/example/sync_book/service/MinioService.java
@@ -5,6 +5,7 @@ import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.errors.MinioException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,6 +19,7 @@ import static com.example.sync_book.util.MinioUtil.getBucketName;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MinioService {
 
     private final MinioClient minioClient;
@@ -29,18 +31,22 @@ public class MinioService {
                     .object(file.getOriginalFilename())
                     .stream(inputStream, file.getSize(), -1)
                     .build());
+            log.info("File '{}' uploaded successfully.", file.getOriginalFilename());
         } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            log.error("Error occurred while uploading file '{}': {}", file.getOriginalFilename(), e.getMessage());
             throw new RuntimeException("Failed to upload file. Please try again.", e);
         }
     }
 
-        public byte[] get(String fileName) {
-            try (InputStream stream = minioClient.getObject(GetObjectArgs.builder()
-                    .bucket(getBucketName())
-                    .object(fileName)
-                    .build())) {
-                return stream.readAllBytes();
+    public byte[] get(String fileName) {
+        try (InputStream stream = minioClient.getObject(GetObjectArgs.builder()
+                .bucket(getBucketName())
+                .object(fileName)
+                .build())) {
+            log.info("File '{}' downloaded successfully.", fileName);
+            return stream.readAllBytes();
         } catch (IOException | MinioException | InvalidKeyException | NoSuchAlgorithmException e) {
+            log.error("Error occurred while downloading file '{}': {}", fileName, e.getMessage());
             throw new RuntimeException("Failed to download file. Please try again.", e);
         }
     }
