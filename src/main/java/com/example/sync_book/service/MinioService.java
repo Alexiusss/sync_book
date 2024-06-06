49875a41885e8dce68b5233dfcff347b7aa0bb34
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-06-06 17:47:22 +0300

    Refactored code and made minor improvements for the Minio controller and service classes

--- src/main/java/com/example/sync_book/service/MinioService.java
+++ src/main/java/com/example/sync_book/service/MinioService.java
@@ -2,49 +2,46 @@ package com.example.sync_book.service;
 
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
-import io.minio.ObjectWriteResponse;
 import io.minio.PutObjectArgs;
-import io.minio.errors.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.minio.errors.MinioException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 import static com.example.sync_book.util.MinioUtil.getBucketName;
 
 @Service
+@RequiredArgsConstructor
 public class MinioService {
 
-    @Autowired
-    MinioClient minioClient;
+    private final MinioClient minioClient;
 
     public void save(MultipartFile file) {
         try (BufferedInputStream inputStream = new BufferedInputStream(file.getInputStream())) {
-            ObjectWriteResponse response = minioClient.putObject(PutObjectArgs.builder()
+            minioClient.putObject(PutObjectArgs.builder()
                     .bucket(getBucketName())
                     .object(file.getOriginalFilename())
                     .stream(inputStream, file.getSize(), -1)
                     .build());
-
-            response.etag();
-        } catch (IOException | ErrorResponseException | InsufficientDataException | InternalException |
-                 InvalidKeyException | InvalidResponseException | NoSuchAlgorithmException | ServerException |
-                 XmlParserException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to upload file. Please try again.", e);
         }
     }
 
-    public byte[] get(String fileName) throws ServerException, InsufficientDataException,
-            ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException,
-            InvalidResponseException, XmlParserException, InternalException {
-        return minioClient.getObject(GetObjectArgs.builder()
-                        .bucket(getBucketName())
-                        .object(fileName)
-                        .build())
-                .readAllBytes();
+        public byte[] get(String fileName) {
+            try (InputStream stream = minioClient.getObject(GetObjectArgs.builder()
+                    .bucket(getBucketName())
+                    .object(fileName)
+                    .build())) {
+                return stream.readAllBytes();
+        } catch (IOException | MinioException | InvalidKeyException | NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to download file. Please try again.", e);
+        }
     }
 }
--- src/main/java/com/example/sync_book/web/MinioController.java
+++ src/main/java/com/example/sync_book/web/MinioController.java
@@ -1,22 +1,15 @@
 package com.example.sync_book.web;
 
 import com.example.sync_book.service.MinioService;
-import io.minio.MinioClient;
-import io.minio.errors.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -26,16 +19,12 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class MinioController {
     public static final String REST_URL = "/api/v1/minio";
 
-    @Autowired
-    MinioClient minioClient;
-
-    @Autowired
-    MinioService minioService;
+    private final MinioService minioService;
 
     @Operation(summary = "Upload the file")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?> upload(@Parameter(description = "File to upload") @RequestParam("file")
+    public ResponseEntity<String> upload(@Parameter(description = "File to upload") @RequestParam("file")
                                     @Schema(type = "string", format = "binary") MultipartFile file) {
         minioService.save(file);
         return ResponseEntity.ok(file.getOriginalFilename());
@@ -43,10 +32,10 @@ public class MinioController {
 
     @GetMapping
     @Operation(summary = "Download the file by its name")
-    public ResponseEntity<?> download(@RequestParam String fileName) throws ServerException, InsufficientDataException,
-            ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException,
-            InvalidResponseException, XmlParserException, InternalException {
-        byte[] file = minioService.get(fileName);
-        return ResponseEntity.ok(file);
+    public ResponseEntity<byte[]> download(@RequestParam String fileName) {
+        byte[] content = minioService.get(fileName);
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(content);
     }
 }
