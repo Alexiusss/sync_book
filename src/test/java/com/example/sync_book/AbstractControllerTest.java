89f3fe8990c6803a5084a1a069ea59e9d40ac693
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-06-11 18:13:24 +0300

    Added Testcontainers

--- src/test/java/com/example/sync_book/AbstractControllerTest.java
+++ src/test/java/com/example/sync_book/AbstractControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.sync_book;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,16 +8,32 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.testcontainers.containers.MinIOContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static com.example.sync_book.util.MinioTestData.initMinioContainer;
+import static com.example.sync_book.util.MinioTestData.setMinioTestProperties;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Testcontainers
 class AbstractControllerTest {
 
-	@Autowired
-	protected MockMvc mockMvc;
+    @Autowired
+    protected MockMvc mockMvc;
 
-	protected ResultActions perform(MockHttpServletRequestBuilder builder) throws Exception {
-		return mockMvc.perform(builder);
-	}
+    @Container
+    private static MinIOContainer minIOContainer = initMinioContainer();
+
+    @BeforeAll
+    static void setUp() {
+        setMinioTestProperties(minIOContainer);
+    }
+
+    protected ResultActions perform(MockHttpServletRequestBuilder builder) throws Exception {
+        return mockMvc.perform(builder);
+    }
 }
--- src/test/java/com/example/sync_book/util/MinioTestData.java
+++ src/test/java/com/example/sync_book/util/MinioTestData.java
@@ -1,11 +1,43 @@
 package com.example.sync_book.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mock.web.MockMultipartFile;
+import org.testcontainers.containers.MinIOContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
 import java.io.IOException;
+import java.time.Duration;
 
+@Slf4j
 public class MinioTestData {
+        private static final String MINIO_IMAGE = "minio/minio";
+    public static final String MINIO_USERNAME = "admin";
+    public static final String MINIO_PASSWORD = "password";
+    public static final int MINIO_PORT = 9000;
     public static final String NOT_FOUND_MESSAGE = "The specified key does not exist.";
+
+    public static MinIOContainer initMinioContainer() {
+        log.info("Initializing MinIO container with image: {}", MINIO_IMAGE);
+        return new MinIOContainer(MINIO_IMAGE)
+                .withUserName(MINIO_USERNAME)
+                .withPassword(MINIO_PASSWORD)
+                .withExposedPorts(MINIO_PORT)
+                .withCommand("server /data")
+                .waitingFor(new HttpWaitStrategy()
+                        .forPath("/minio/health/ready")
+                        .forPort(MINIO_PORT)
+                        .withStartupTimeout(Duration.ofSeconds(10)));
+    }
+
+    public static void setMinioTestProperties(MinIOContainer container) {
+        System.setProperty("MINIO_URL", container.getHost());
+        System.setProperty("MINIO_PORT", container.getMappedPort(MINIO_PORT).toString());
+        System.setProperty("MINIO_ROOT_USER", MINIO_USERNAME);
+        System.setProperty("MINIO_ROOT_PASSWORD", MINIO_PASSWORD);
+        log.info("MinIO container properties set: URL=http://{}:{}, User={}, Password={}",
+                container.getHost(), container.getMappedPort(MINIO_PORT),
+                MINIO_USERNAME, MINIO_PASSWORD);
+    }
 
     public static MockMultipartFile getNewFile(String fileName, String mediaType) throws IOException {
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
