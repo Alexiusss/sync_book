b111a4ca43a585fb9095f75635952e4dd09a6df6
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-06-17 20:28:54 +0300

    Implemented i18n support

--- src/main/java/com/example/sync_book/config/AppConfig.java
+++ src/main/java/com/example/sync_book/config/AppConfig.java
@@ -3,12 +3,22 @@ package com.example.sync_book.config;
 import com.example.sync_book.util.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 public class AppConfig {
     @Autowired
     private void setMapper(ObjectMapper objectMapper) {
         JsonUtil.setMapper(objectMapper);
+    }
+
+    @Bean
+    protected ResourceBundleMessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
--- src/main/java/com/example/sync_book/model/Genre.java
+++ src/main/java/com/example/sync_book/model/Genre.java
@@ -1,5 +1,8 @@
 package com.example.sync_book.model;
 
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
 public enum Genre {
     FICTION,
     NON_FICTION,
@@ -9,4 +12,14 @@ public enum Genre {
     DETECTIVE,
     ADVENTURE,
     GUIDE;
+
+    private static ResourceBundleMessageSource messageSource;
+
+    public static void setMessageSource(ResourceBundleMessageSource messageSource) {
+        Genre.messageSource = messageSource;
+    }
+
+    public String getLocalizedName() {
+        return messageSource.getMessage("genre." + this.name().toLowerCase(), null, LocaleContextHolder.getLocale());
+    }
 }
