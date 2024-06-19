24fbd549402e0dabac57733e764692d12d052e15
Alexey Boyarinov <boyarinovalexey@gmail.com>
2024-06-19 21:42:52 +0300

    Added invalid tests to PublisherControllerTest

--- src/test/java/com/example/sync_book/util/PublisherTestData.java
+++ src/test/java/com/example/sync_book/util/PublisherTestData.java
@@ -6,6 +6,9 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class PublisherTestData {
 
+    public static final int NOT_FOUND_ID = 1000;
+    public static final String NOT_FOUND_MESSAGE = "Entity with id=" + NOT_FOUND_ID + " not found";
+
     public static PublisherTo getNew() {
         return new PublisherTo(null, "name", 2005, "city name");
     }
--- src/test/java/com/example/sync_book/web/PublisherControllerTest.java
+++ src/test/java/com/example/sync_book/web/PublisherControllerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static com.example.sync_book.util.PublisherTestData.getNew;
+import static com.example.sync_book.util.PublisherTestData.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PublisherControllerTest extends AbstractControllerTest {
@@ -22,5 +24,30 @@ class PublisherControllerTest extends AbstractControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(JsonUtil.writeValue(publisherUnsafe)))
                 .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void createInvalid() throws Exception {
+        PublisherTo publisherInvalid = getNew();
+        publisherInvalid.setName("");
+
+        perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(publisherInvalid)))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/" + NOT_FOUND_ID))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("detail", equalTo(NOT_FOUND_MESSAGE)));
+    }
+
+    @Test
+    void deleteNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + "/" + NOT_FOUND_ID))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("detail", equalTo(NOT_FOUND_MESSAGE)));
     }
 }
